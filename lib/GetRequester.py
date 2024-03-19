b800826d5c9496285ee6ae99c80445f8b07e89ff
Anthony <gachinigaithuma@gmail.com>
2024-03-19 16:36:56 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL = self.url
+
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        data = json.loads(self.get_response_body())
+
+        return data
